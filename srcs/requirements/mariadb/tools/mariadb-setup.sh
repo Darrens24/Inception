37315dc94803cd7187072faf5f49af25fa42ee27
Darrens24 <edouard.leleux@gmail.com>
2023-07-20 10:38:42 +0200

    bug mariadb on compose

---
+++ srcs/requirements/mariadb/tools/mariadb-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#https://wiki.alpinelinux.org/wiki/MariaDB
+
+chown -R mysql:mysql /var/lib/mysql
+/usr/bin/mysql_install_db
+/usr/bin/mysqld & sleep 3
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;"
+mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USR'@'localhost' IDENTIFIED BY '$MYSQL_PSSWD';"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USR'@'localhost' IDENTIFIED BY '$MYSQL_PSSWD';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PSSWD';"
+mysql -e "FLUSH PRIVILEGES;"
+
+pkill mysqld
+/usr/bin/mysqld 
